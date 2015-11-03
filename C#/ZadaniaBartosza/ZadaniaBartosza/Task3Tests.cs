5eac40ee1c913e637ade2cefb700321d1225f46b
zchpit <rabiniak.piotr@gmail.com>
2015-11-03 22:57:57 +0100

    remove unnessesary comment

--- C#/ZadaniaBartosza/ZadaniaBartosza/Task3Tests.cs
+++ C#/ZadaniaBartosza/ZadaniaBartosza/Task3Tests.cs
@@ -14,7 +14,6 @@ namespace ZadaniaBartosza
 		[Test]
 		public void CommonDivisorExists()
 		{
-			//results taken from https://pl.wikipedia.org/wiki/Liczba_pierwsza
 			Assert.AreEqual(2,  task.GetSmallestCommonDivisor(2,4));
 			Assert.AreEqual(3,  task.GetSmallestCommonDivisor(3,9));
 			Assert.AreEqual(2,  task.GetSmallestCommonDivisor(2,8));
@@ -25,7 +24,6 @@ namespace ZadaniaBartosza
 		[Test]
 		public void CommonDivisorDontExists()
 		{
-			//results taken from https://pl.wikipedia.org/wiki/Liczba_pierwsza
 			Assert.AreEqual(-1,  task.GetSmallestCommonDivisor(3,5));
 			Assert.AreEqual(-1,  task.GetSmallestCommonDivisor(5,3));
 			Assert.AreEqual(-1,  task.GetSmallestCommonDivisor(15,4));
