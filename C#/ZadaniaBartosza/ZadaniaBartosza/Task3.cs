4dc238b0457c5f4cbc5b3433ad25ee78f13f9a80
zchpit <rabiniak.piotr@gmail.com>
2015-11-03 22:51:05 +0100

    added task3 -> smallest common divisor

---
+++ C#/ZadaniaBartosza/ZadaniaBartosza/Task3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ZadaniaBartosza
+{
+	public class Task3
+	{
+		public Task3 ()
+		{
+			
+		}
+		public int GetSmallestCommonDivisor(int x, int y)
+		{
+			int dontExists = -1;
+
+			for (int i = 2; i < Int32.MaxValue; i++) {
+				int restX = x % i;
+				int restY = y % i;
+
+				if (restX == 0 && restY == 0)
+					return i;
+				if (i > x || i > y)
+					return dontExists;
+			}
+			return dontExists;
+		}
+	}
+}
+
---
+++ C#/ZadaniaBartosza/ZadaniaBartosza/Task3Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ZadaniaBartosza
+{
+	[TestFixture]
+	public class Task3Tests
+	{
+		Task3 task;
+		public Task3Tests ()
+		{
+			task = new Task3();
+		}
+		[Test]
+		public void CommonDivisorExists()
+		{
+			//results taken from https://pl.wikipedia.org/wiki/Liczba_pierwsza
+			Assert.AreEqual(2,  task.GetSmallestCommonDivisor(2,4));
+			Assert.AreEqual(3,  task.GetSmallestCommonDivisor(3,9));
+			Assert.AreEqual(2,  task.GetSmallestCommonDivisor(2,8));
+			Assert.AreEqual(2,  task.GetSmallestCommonDivisor(4,16));
+			Assert.AreEqual(3,  task.GetSmallestCommonDivisor(9,18));
+			Assert.AreEqual(17,  task.GetSmallestCommonDivisor(17,34));
+		}
+		[Test]
+		public void CommonDivisorDontExists()
+		{
+			//results taken from https://pl.wikipedia.org/wiki/Liczba_pierwsza
+			Assert.AreEqual(-1,  task.GetSmallestCommonDivisor(3,5));
+			Assert.AreEqual(-1,  task.GetSmallestCommonDivisor(5,3));
+			Assert.AreEqual(-1,  task.GetSmallestCommonDivisor(15,4));
+		}
+	}
+}
+
